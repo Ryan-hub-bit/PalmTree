2e8dfff1c5399534b303fa121861534534314a00
ryan <liu864579887@gmail.com>
2026-01-21 18:19:53 +0000

    fdafa

--- extern/jTrans_instr/pretrain/run_instr_pretrain_hpc.sh
+++ extern/jTrans_instr/pretrain/run_instr_pretrain_hpc.sh
@@ -9,7 +9,7 @@
 #          ./run_instr_pretrain_hpc.sh 1.0    # Use 100% of data (default)
 
 # Data ratio (default: 1.0 = 100% of data)
-DATA_RATIO="${1:-0.00001}"  # Changed from 0.0001 to 1.0 for full training
+DATA_RATIO="${1:-1.0}"  # Changed from 0.0001 to 1.0 for full training
 
 # Use all available GPUs (default: 0,1,2,3 for 4 GPUs)
 # SLURM will set CUDA_VISIBLE_DEVICES automatically, but if running locally, set it here
@@ -33,7 +33,7 @@ INTERMEDIATE_SIZE=3072
 MAX_INSTRUCTIONS=201
 
 # Training hyperparameters
-BATCH_SIZE=64  # Reduced from 256 to avoid OOM. Increase gradually if you have more GPU memory
+BATCH_SIZE=256  # Reduced from 256 to avoid OOM. Increase gradually if you have more GPU memory
 LEARNING_RATE=1e-4
 NUM_EPOCHS=10
 WARMUP_STEPS=10000
