2fe067e5daf9190cca9becdb6707f51b0ef49060
ryan <liu864579887@gmail.com>
2026-01-25 04:07:12 +0000

    dfaffaf

--- extern/jTrans_instr/pretrain/run_instr_pretrain_hpc.sh
+++ extern/jTrans_instr/pretrain/run_instr_pretrain_hpc.sh
@@ -9,7 +9,7 @@
 #          ./run_instr_pretrain_hpc.sh 1.0    # Use 100% of data (default)
 
 # Data ratio (default: 1.0 = 100% of data)
-DATA_RATIO="${1:-0.00001}"  # Changed from 0.0001 to 1.0 for full training
+DATA_RATIO="${1:-1.0}"  # Changed from 0.0001 to 1.0 for full training
 
 # Use all available GPUs (default: 0,1,2,3 for 4 GPUs)
 # SLURM will set CUDA_VISIBLE_DEVICES automatically, but if running locally, set it here
