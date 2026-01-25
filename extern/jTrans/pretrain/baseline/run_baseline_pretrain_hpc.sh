43ef9d0dcec8352c6af191638b7bd3bb44c1f3a4
ryan <liu864579887@gmail.com>
2026-01-25 01:22:03 +0000

    retrain baseline

--- extern/jTrans/pretrain/baseline/run_baseline_pretrain_hpc.sh
+++ extern/jTrans/pretrain/baseline/run_baseline_pretrain_hpc.sh
@@ -41,7 +41,7 @@ python3 train_baseline.py \
   --train_path /work/kliu14/jtransdata/baseline_pretrain.txt \
   --tokenizer_path . \
   --output_dir /work/kliu14/jtransoutput/baseline_pretrain \
-  --batch_size 128 \
+  --batch_size 64 \
   --learning_rate 1e-4 \
   --num_epochs 10 \
   --warmup_steps 10000 \
